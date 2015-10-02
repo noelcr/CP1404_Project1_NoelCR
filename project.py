67986c212b8820d757b13fd632874c06b251e2bb
noelcr <noel.chillerroberts@my.jcu.edu.au>
2015-10-02 10:29:30 +1000

    Created first steps for the Currency module function "convert".

--- project.py
+++ project.py
@@ -1,1 +1,18 @@
+import web_utility
 
+amount = str('5')
+first_country = 'EUR'
+sec_country = 'USD'
+
+url_string = "https://www.google.com/finance/converter?a=1&from=AUD&to=JPY"
+
+url_string = url_string.replace('1', amount)
+url_string = url_string.replace('AUD', first_country)
+url_string = url_string.replace('JPY', sec_country)
+
+
+print (url_string)
+
+result = web_utility.load_page(url_string)
+print(result[result.index('result'):])
+url_string = "https://www.google.com/finance/converter?a=1&from=AUD&to=JPY"

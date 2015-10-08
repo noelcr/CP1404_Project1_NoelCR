cdd5698fd8268e6671b72f345a7bb38c95f09f5d
noelcr <noel.chillerroberts@my.jcu.edu.au>
2015-10-08 17:13:15 +1000

    (trip.py) country class now has working method that generates the symbol next to an amount (called symbol_assign)

--- trip.py
+++ trip.py
@@ -1,40 +1,55 @@
-class Error():
-    try:
-        print(1/1)
-    except ZeroDivisionError as error:
-        print(error)
-    print("After try/except")
+# class Error():
+#     try:
+#         print(1 / 1)
+#     except ZeroDivisionError as error:
+#         print(error)
+#     print("After try/except")
+
 
 class Country():
     def __init__(self, name, currency_code, currency_symbol):
-        self.name=""
-        self.currency_code=""
-        self.currency_symbol=""
+        self.name = name
+        self.currency_code = currency_code
+        self.currency_symbol = currency_symbol
         print(name, currency_code, currency_symbol)
-        symbol_list = []
-        symbol_list.append(name)
-        symbol_list.append(currency_code)
-        symbol_list.append(currency_symbol)
-        #print(symbol_list)
-        amount =  int(input("amount: "))
-        print('{}{}'.format(symbol_list[-1],amount))
+        #print(currency_symbol)
+
 
     def __str__(self):
         return "{}, {}, {}".format(self.name, self.currency_code, self.currency_symbol)
 
 
-class Details:
+    def symbol_assign(self, amount):
+        #print(self.currency_symbol, "test")
+        #amount = 10#int(input("amount: "))
+        print('{}{}'.format(self.currency_symbol, amount))
+
+
+
+
+class Details():
     def __init__(self):
         self.locations = []
 
     def add(self, country_name="", start_date="", end_date=""):
-        print("A date string conforms to this format: YYYY/MM/DD")
-        self.country_name=""
-        self.start_date=""
-        self.end_date=""
-        print(self.country_name, self.start_date, self.end_date)
+        #print("A date string conforms to this format: YYYY/MM/DD")
+        return country_name, start_date, end_date
 
-Country('Germany', 'EUR', '€')
+    def current_country(self, date_string):
+        print()
+
+    def is_empty(self):
+        print()
+
+#Country('Germany', 'EUR', '€')
 #print(Country)#why doesn't this work?
 
 
+test_country1 = Country('bananaland', 'BNL', '€')
+
+
+# test_details = Details.add(Details, 'Germany', '1997/01/25', '1997/01/30')
+# print(test_details)
+
+
+test_country1.symbol_assign('10')
